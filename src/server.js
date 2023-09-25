7d48cbc4b82ac44091fda3ca18040963fe013be8
ehdtjs0612 <inko51366@naver.com>
2023-09-25 17:32:34 +0900

    connect router at server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const redisClient = require("../config/database/redis");
+
+const accountApi = require("./routes/account");
+const authApi = require("./routes/auth");
 const errorHandling = require("./middleware/errorHandling");
+
+redisClient.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/account", accountApi);
+app.use("/auth", authApi);
 
 app.use(errorHandling());
 
