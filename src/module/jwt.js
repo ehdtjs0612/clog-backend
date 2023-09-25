e4029b03c2ae2c9e9c2e7c4187c87ec1a7c805b6
ehdtjs0612 <inko51366@naver.com>
2023-09-25 17:27:24 +0900

    Feat: add jwt config module

---
+++ src/module/jwt.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const { secretKey, accessTokenOption } = require("../../config/jwtSetting");
+
+const userSign = (user) => {
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+
+    return jwt.sign(payload, secretKey, accessTokenOption);
+}
+
+module.exports = {
+    userSign
+};
