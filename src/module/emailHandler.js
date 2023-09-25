d9ebbfcc69ee4d0997a0ba3666fc9defd88d76e4
ehdtjs0612 <inko51366@naver.com>
2023-09-25 20:56:41 +0900

    Feat: add emailHandler module

---
+++ src/module/emailHandler.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const nodeMailer = require("nodemailer");
+const redisClient = require("../../config/database/redis");
+
+const sendVerifyEmail = async (email) => {
+    const transporter = nodeMailer.createTransport({
+        service: "naver",
+        host: "smtp.naver.com",
+        port: 587,
+        auth: {
+            user: process.env.NODEMAILER_ID,
+            pass: process.env.NODEMAILER_PW
+        }
+    });
+    const authCode = Math.floor(Math.random() * 89999) + 10000;
+    redisClient.set(email, authCode.toString());
+    redisClient.expire(email, 180);
+
+    const option = {
+        from: process.env.NODEMAILER_ID,
+        to: email,
+        subject: "인증번호입니당",
+        text: `인증번호: ${authCode}`,
+    }
+
+    return await transporter.sendMail(option);
+}
+
+module.exports = {
+    sendVerifyEmail,
+};
