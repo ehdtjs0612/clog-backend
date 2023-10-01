c9c9bd343228ae748fcd996a0bf15d820d796ee2
ehdtjs0612 <inko51366@naver.com>
2023-10-01 18:46:25 +0900

    Refactor: declaration club information global variable

--- src/module/global.js
+++ src/module/global.js
@@ -6,4 +6,15 @@ module.exports = {
     // auth
     certifiedLength: 5,
     certifiedExpireTime: 180,
+
+    // club
+    maxClubCoverLength: 500,
+    maxClubBelongLength: 5,
+    maxClubBigCategoryLength: 5,
+    maxClubSmallCategoryLength: 5,
+
+    // position
+    president: 0,
+    manager: 1,
+    member: 2
 }
