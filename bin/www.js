894cd800e93c98034d4c8134ef3e927e60b4cbf4
ehdtjs0612 <inko51366@naver.com>
2023-10-06 16:31:59 +0900

    Rename: rename variable router to app

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
-const router = require("../src/server");
+const app = require("../src/server");
 const PORT = process.env.PORT;
 
-router.listen(PORT, (req, res) => {
+app.listen(PORT, (req, res) => {
     console.log(PORT + "번에서 실행");
 });
