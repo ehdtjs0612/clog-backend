8156173bf870de1a00e28fdc48a64e9f7903d746
ehdtjs0612 <inko51366@naver.com>
2023-10-07 13:40:55 +0900

    Feat: add club-board api

--- src/routes/board.js
+++ src/routes/board.js
@@ -4,7 +4,7 @@ const pool = require('../../config/database/postgresql');
 const loginAuth = require('../middleware/auth/loginAuth');
 const managerAuth = require('../middleware/auth/managerAuth');
 const validate = require('../module/validation');
-const CONSTRAINT = require('../module/constraint');
+const { club } = require("../module/global");
 const { board } = require("../module/global");
 const { BadRequestException } = require('../module/customError');
 
@@ -45,22 +45,26 @@ router.post("/", loginAuth, managerAuth, async (req, res, next) => {
     try {
         validate(name, "name").checkInput().checkLength(1, board.MAX_BOARD_LENGTH);
 
-        const createBoardSql = `INSERT INTO 
-                                            club_board_tb (club_id, name) 
-                                SELECT 
+        const createBoardSql = `INSERT INTO
+                                            club_board_tb (club_id, name)
+                                SELECT
                                             $1, $2
-                                WHERE NOT EXISTS 
-                                            (SELECT 1 FROM club_board_tb WHERE name = $3)
-                                RETURNING
-                                            id`;
-        const createBoardParam = [clubId, name, name];
+                                WHERE 
+                                            (SELECT 
+                                                    COUNT(*) 
+                                             FROM
+                                                    club_board_tb 
+                                             WHERE 
+                                                    club_id = $3) < $4
+                                RETURNING id`;
+        const createBoardParam = [clubId, name, clubId, club.MAX_BOARD_COUNT];
         const createBoardData = await pool.query(createBoardSql, createBoardParam);
         if (createBoardData.rowCount !== 0) {
             result.message = "게시판 생성 성공";
             result.data = createBoardData.rows[0].id;
             return res.send(result);
         }
-        throw new BadRequestException("중복된 게시판 이름이 존재합니다");
+        throw new BadRequestException(`게시판의 최대 수는 ${club.MAX_BOARD_COUNT}개입니다`)
 
     } catch (error) {
         next(error);
