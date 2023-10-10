ca71322b1758a16f843ac0b8dd29d639c0b18b33
ehdtjs0612 <inko51366@naver.com>
2023-10-10 22:08:00 +0900

    Refactor: refactor authCheck middleware

--- src/middleware/auth/authCheck.js
+++ src/middleware/auth/authCheck.js
@@ -5,7 +5,7 @@ const { ForbbidenException } = require('../../module/customError');
 module.exports = (position) => {
     return async (req, res, next) => {
         const userId = req.decoded.id;
-        const clubId = req.params.clubId ?? req.body.clubId;
+        const clubId = req.params.clubId || req.body.clubId;
 
         try {
             validate(clubId, "clubId").checkInput().isNumber();
@@ -18,15 +18,18 @@ module.exports = (position) => {
                                                 account_id = $1 AND club_id = $2`;
             const selectPositionParam = [userId, clubId];
             const selectPositionData = await pool.query(selectPositionSql, selectPositionParam);
-            if (selectPositionData.rowCount !== 0) {
-                if (selectPositionData.rows[0].position <= position) {
-                    return next();
-                }
-            }
-            throw new ForbbidenException("관리자 권한이 필요합니다");
 
+            if (selectPositionData.rowCount === 0) {
+                throw new ForbbidenException("로그인 사용자가 동아리에 가입된 사용자가 아닙니다");
+            }
+
+            if (selectPositionData.rows[0].position > position) {
+                throw new ForbbidenException("관리자 권한이 필요합니다");
+            }
         } catch (error) {
             next(error);
         }
+
+        next();
     }
 }
