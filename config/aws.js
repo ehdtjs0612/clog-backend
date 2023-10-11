ec2e4dac1a6e0e131abcdcadb788b64d86f5f98c
ehdtjs0612 <inko51366@naver.com>
2023-10-11 18:01:25 +0900

    Refactor: separate s3 configuration

--- config/aws.js
+++ config/aws.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
-
-const AWS = require("aws-sdk");
-AWS.config.update({
+const awsConfig = {
     region: 'ap-northeast-2',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECERET_ACCESS_KEY
-});
+};
 
-module.exports = AWS;
+module.exports = awsConfig;
