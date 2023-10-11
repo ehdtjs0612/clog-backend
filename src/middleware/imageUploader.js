23c344d6d1f1a79d6cce15e8d786bf6eb76f2c55
ehdtjs0612 <inko51366@naver.com>
2023-10-11 23:24:38 +0900

    Feat: add image upload api

--- src/middleware/imageUploader.js
+++ src/middleware/imageUploader.js
@@ -6,6 +6,7 @@ const multerS3 = require("multer-s3-transform");
 const path = require("path");
 const awsConfig = require('../../config/aws');
 const allowedExtensions = ['.png', '.jpg', '.jpeg'];
+const { IMAGE } = require("../module/global");
 const { BadRequestException } = require("../module/customError");
 
 AWS.config.update(awsConfig);
@@ -20,12 +21,13 @@ const clubProfileUpload = multer({
             if (!allowedExtensions.includes(extension)) {
                 return callback(new BadRequestException("파일 형식을 확인해주세요"));
             }
+            console.log(file);
             callback(null, `club/profile/${Date.now()}_${file.originalname}`);
         },
         acl: "public-read-write"
     }),
     limits: {
-        fileSize: 1 * 1024 * 1024, // 1MB
+        fileSize: 5 * 1024 * 1024, // 5MB
     }
 });
 
@@ -44,7 +46,64 @@ const clubBannerUpload = multer({
         acl: "public-read-write"
     }),
     limits: {
-        fileSize: 5 * 1024 * 1024, // 1MB
+        fileSize: 5 * 1024 * 1024, // 5MB
+    }
+});
+
+const clubPostUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, callback) => {
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new BadRequestException("파일 형식을 확인해주세요"));
+            }
+            callback(null, `club/post/general/${Date.now()}_${file.originalname}`);
+        },
+        acl: "public-read-write"
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    }
+});
+
+const clubPromotionUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, callback) => {
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new BadRequestException("파일 형식을 확인해주세요"));
+            }
+            callback(null, `club/post/promotion/${Date.now()}_${file.originalname}`);
+        },
+        acl: "public-read-write"
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    }
+});
+
+const clubNoticeUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, callback) => {
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new BadRequestException("파일 형식을 확인해주세요"));
+            }
+            callback(null, `club/post/notice/${Date.now()}_${file.originalname}`);
+        },
+        acl: "public-read-write"
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
     }
 });
 
@@ -52,8 +111,13 @@ module.exports = {
     clubProfileUploader: () => {
         return async (req, res, next) => {
             clubProfileUpload.single("clubProfile")(req, res, (err) => {
-                if (err instanceof multer.MulterError) {
-                    return next(new BadRequestException("멀터에러 님이잘못함"));
+                // 1. 단일 이미지가 아닐 경우
+                if (err?.code === "LIMIT_UNEXPECTED_FILE") {
+                    return next(new BadRequestException(`단일 이미지만 업로드 가능합니다`));
+                }
+                // 2. 이미지 용량이 큰 경우
+                if (err?.code === "LIMIT_FILE_SIZE") {
+                    return next(new BadRequestException(`이미지 용량이 너무 큽니다`));
                 }
                 if (err) {
                     return next(err);
@@ -67,16 +131,80 @@ module.exports = {
     clubBannerUploader: () => {
         return async (req, res, next) => {
             clubBannerUpload.single("clubBanner")(req, res, (err) => {
-                if (err instanceof multer.MulterError) {
-                    // 1. 이미지 용량이 큰 경우?
-                    return next(new BadRequestException("파일 용량은 5MB 이하여야합니다"));
+                // 1. 단일 이미지가 아닐 경우
+                if (err?.code === "LIMIT_UNEXPECTED_FILE") {
+                    return next(new BadRequestException(`단일 이미지만 업로드 가능합니다`));
+                }
+                // 2. 이미지 용량이 큰 경우
+                if (err?.code === "LIMIT_FILE_SIZE") {
+                    return next(new BadRequestException(`이미지 용량이 너무 큽니다`));
                 }
                 if (err) {
                     return next(err);
                 }
-                console.log(`clubProfileUpload 실행`); // 개발환경 전용
+                console.log(`clubBannerUploader 실행`); // 개발환경 전용
                 next();
             });
         }
-    }
+    },
+
+    postUploader: () => {
+        return async (req, res, next) => {
+            clubPostUpload.array("postImages", IMAGE.MAX_POST_COUNT)(req, res, (err) => {
+                // 1. 이미지 개수를 초과한 경우
+                if (err?.code === "LIMIT_UNEXPECTED_FILE") {
+                    return next(new BadRequestException(`이미지 개수는 ${IMAGE.MAX_NOTICE_COUNT}개 이하여야합니다`));
+                }
+                // 2. 이미지 용량이 큰 경우
+                if (err?.code === "LIMIT_FILE_SIZE") {
+                    return next(new BadRequestException(`이미지 용량이 너무 큽니다`));
+                }
+                if (err) {
+                    return next(err);
+                }
+                console.log(`clubPostUploader 실행`); // 개발환경 전용
+                next();
+            });
+        }
+    },
+
+    promotionUploader: () => {
+        return async (req, res, next) => {
+            clubPromotionUpload.array("postImages", IMAGE.MAX_PROMOTION_COUNT)(req, res, (err) => {
+                // 1. 이미지 개수를 초과한 경우
+                if (err?.code === "LIMIT_UNEXPECTED_FILE") {
+                    return next(new BadRequestException(`이미지 개수는 ${IMAGE.MAX_PROMOTION_COUNT}개 이하여야합니다`));
+                }
+                // 2. 이미지 용량이 큰 경우
+                if (err?.code === "LIMIT_FILE_SIZE") {
+                    return next(new BadRequestException(`이미지 용량이 너무 큽니다`));
+                }
+                if (err) {
+                    return next(err);
+                }
+                console.log(`clubPromotionUploader 실행`); // 개발환경 전용
+                next();
+            });
+        }
+    },
+
+    noticeUploader: () => {
+        return async (req, res, next) => {
+            clubNoticeUpload.array("postImages", IMAGE.MAX_NOTICE_COUNT)(req, res, (err) => {
+                // 1. 이미지 개수를 초과한 경우
+                if (err?.code === "LIMIT_UNEXPECTED_FILE") {
+                    return next(new BadRequestException(`이미지 개수는 ${IMAGE.MAX_NOTICE_COUNT}개 이하여야합니다`));
+                }
+                // 2. 이미지 용량이 큰 경우
+                if (err?.code === "LIMIT_FILE_SIZE") {
+                    return next(new BadRequestException(`이미지 용량이 너무 큽니다`));
+                }
+                if (err) {
+                    return next(err);
+                }
+                console.log(`clubNoticeUploader 실행`); // 개발환경 전용
+                next();
+            });
+        }
+    },
 }
