acf5d188e7d4db1bbf66445cc2446215d8170585
ehdtjs0612 <inko51366@naver.com>
2023-10-14 18:05:42 +0900

    Feat: add modify reply api

--- src/routes/reply.js
+++ src/routes/reply.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const pool = require('../../config/database/postgresql');
 const loginAuth = require('../middleware/auth/loginAuth');
 const { BadRequestException } = require('../module/customError');
-const { REPLY } = require('../module/global');
+const { REPLY, POSITION } = require('../module/global');
 const CONSTRAINT = require("../module/constraint");
 const validate = require('../module/validation');
 
@@ -208,6 +208,81 @@ router.post("/", loginAuth, async (req, res, next) => {
         if (error.constraint === CONSTRAINT.FK_COMMENT_TO_CLUB_REPLY_TB) {
             throw new BadRequestException("해당하는 댓글이 존재하지 않습니다");
         }
+        return next(error);
+    }
+    res.send(result);
+});
+
+// 대댓글 수정 api
+// 권한: 본인이거나 해당 동아리의 운영진이거나
+router.put("/", loginAuth, async (req, res, next) => {
+    const userId = req.decoded.id;
+    const { replyId, content } = req.body;
+    const result = {
+        message: "",
+        data: {}
+    };
+
+    try {
+        validate(replyId, "replyId").checkInput().isNumber();
+        validate(content, "content").checkInput().checkLength(1, REPLY.MAX_REPLY_CONTENT_LENGTH);
+
+        // 권한 체크
+        const selectAuthSql = `SELECT
+                                    club_reply_tb.account_id AS "accountId",
+                                    (
+                                        SELECT
+                                            club_member_tb.position
+                                        FROM
+                                            club_member_tb
+                                        WHERE
+                                            club_member_tb.account_id = $1
+                                        AND
+                                            club_member_tb.club_id = club_tb.id
+                                    ) AS "position"
+                                FROM
+                                    club_reply_tb
+                                JOIN
+                                    club_comment_tb
+                                ON
+                                    club_reply_tb.club_comment_id = club_comment_tb.id
+                                JOIN
+                                    club_post_tb
+                                ON
+                                    club_comment_tb.club_post_id = club_post_tb.id
+                                JOIN
+                                    club_board_tb
+                                ON
+                                    club_post_tb.club_board_id = club_board_tb.id
+                                JOIN
+                                    club_tb
+                                ON
+                                    club_board_tb.club_id = club_tb.id
+                                WHERE
+                                    club_reply_tb.id = $2`;
+        const selectAuthParam = [userId, replyId];
+        const selectAuthData = await pool.query(selectAuthSql, selectAuthParam);
+        if (selectAuthData.rowCount === 0) {
+            throw new BadRequestException("해당하는 댓글이 존재하지 않습니다");
+        }
+        if (selectAuthData.rows[0].position === null) {
+            throw new BadRequestException("동아리에 가입하지 않은 사용자입니다");
+        }
+        if (selectAuthData.rows[0].accountId !== userId && selectAuthData.rows[0].position >= POSITION.MANAGER) {
+            throw new BadRequestException("수정 권한이 존재하지 않습니다");
+        }
+
+        // 댓글 수정 시작
+        const updateReplySql = `UPDATE
+                                    club_reply_tb
+                                SET
+                                    content = $1
+                                WHERE
+                                    id = $2`;
+        const updateReplyParam = [content, replyId];
+        await pool.query(updateReplySql, updateReplyParam);
+
+    } catch (error) {
         return next(error);
     }
     res.send(result);
